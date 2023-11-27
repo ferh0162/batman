4dc3839503d28cece6439ca8aa21f52736e9a98b
ferh0162 <ferh0162@stud.kea.dk>
2023-11-28 00:20:47 +0100

    Har 2 bats

--- App.js
+++ App.js
@@ -1,200 +1,264 @@
 import React, { useEffect, useState } from "react";
-import { Button, Dimensions, View } from "react-native";
+import { Button, Dimensions, View, Text, StyleSheet } from "react-native";
 import { GameEngine } from "react-native-game-engine";
-import {DeviceMotion} from 'expo-sensors'
-import {Audio} from 'expo-av'
-
+import { DeviceMotion } from "expo-sensors";
+import { Audio } from "expo-av";
 
 export default function App() {
-
   const { width, height } = Dimensions.get("window");
-  const [isAvailable, setIsAvailable] = useState(false)
-  const [ motionData, setMotionData] = useState(null)
-  const [isRunning, setIsRunning] = useState(true)
-  const [sound, setSound] = useState(null)
-  const [isRecording, setRecording] = useState(null)
+  const [isAvailable, setIsAvailable] = useState(false);
+  const [motionData, setMotionData] = useState(null);
+  const [isRunning, setIsRunning] = useState(true);
+  const [timer, setTimer] = useState(0);
+  const [highscore, setHighscore] = useState(0);
+
+  const ball = {
+    position: {
+      x: width / 2 - 25,
+      y: height / 2 - 25,
+    },
+    size: 50,
+    velocity: {
+      x: 0.2,
+      y: 0.2,
+    },
+    renderer: (props) => {
+      const { position, size } = props;
+      return (
+        <View
+          style={{
+            backgroundColor: "red",
+            position: "absolute",
+            left: position.x,
+            top: position.y,
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+          }}
+        ></View>
+      );
+    },
+  };
+
+  const bat = {
+    position: {
+      x: width / 2,
+      y: height,
+    },
+    size: 100,
+    renderer: (props) => {
+      const { position, size } = props;
+      return (
+        <View
+          style={{
+            backgroundColor: "black",
+            position: "absolute",
+            left: position.x, //Sætter battet i midten
+            top: height - 60,
+            width: size,
+            height: size / 5,
+            borderRadius: size / 2,
+          }}
+        ></View>
+      );
+    },
+  };
+
+  const topBat = {
+    position: {
+      x: width / 2,
+      y: height,
+    },
+    size: 100,
+    renderer: (props) => {
+      const { position, size } = props;
+      return (
+        <View
+          style={{
+            backgroundColor: "black",
+            position: "absolute",
+            left: position.x, //Sætter battet i midten
+            top: 60,
+            width: size,
+            height: size / 5,
+            borderRadius: size / 2,
+          }}
+        ></View>
+      );
+    },
+  };
   useEffect(() => {
-    return sound ? () => {
-      sound.unloadAsync()} : undefined
-    }, [sound])
-
-
-const ball = {
-  position: {
-    x: width / 2 - 25,
-    y: height / 2 - 25,
-  },
-  size: 50,
-  velocity: {
-    x: 0.2,
-    y: 0.2,
-  },
-  renderer: (props) => {
-    const { position, size } = props;
-    return (
-      <View
-        style={{
-          backgroundColor: "red",
-          position: "absolute",
-          left: position.x,
-          top: position.y,
-          width: size,
-          height: size,
-          borderRadius: size / 2,
-        }}
-      ></View>
-    );
-  },
-};
-
-const bat = {
-  position: {
-    x: width / 2,
-    y: height,
-  },
-  size: 100,
-  renderer: (props) => {
-    const { position, size } = props;
-    return (
-      <View
-        style={{
-          backgroundColor: "black",
-          position: "absolute",
-          left: position.x, //Sætter battet i midten
-          top: height - 60,
-          width: size,
-          height: size/5,
-          borderRadius: size / 2,
-        }}
-      ></View>
-    );
-  },
-};
-
-useEffect(() =>{
-  async function subscribe(){
-    const available = await DeviceMotion.isAvailableAsync()
-    setIsAvailable(available)
-    //gem i usestate
-    if (available) { //Vi bruger availabe og ikke isAvailable, da den ikke kan nå at sætte og hente den nye værdi
-      DeviceMotion.setUpdateInterval(20) // 20 millisekund pause imellem hver update
-      DeviceMotion.addListener(deviceMotionData =>{
-        setMotionData(deviceMotionData)
-      })
-    }
-  }
-  subscribe()
-  return () => {
-    DeviceMotion.removeAllListeners()
-  }
-},[]) // skal kun køre en gang derfor [], det er en mekanisme
-
-const update = (entities, { time }) => {
-  const batEntity = entities.bat;
-  const ballEntity = entities.ball;
-
-  let extra = 5.0 * motionData.rotation.beta / 1.5
-  if (isNaN(extra)) { extra = 0.0}
-
-  ballEntity.position.x += ballEntity.velocity.x * time.delta * (1+ extra);
-  ballEntity.position.y += ballEntity.velocity.y * time.delta;
-
-  if ( ballEntity.position.x < 0){ //venstre side
-    ballEntity.velocity.x = Math.abs(ballEntity.velocity.x)
-  }
-
-  if (
-    ballEntity.position.x + ballEntity.size > width) { //højre side
-    ballEntity.velocity.x = -1 * Math.abs(ballEntity.velocity.x);
-  }
-
-  if (
-    ballEntity.position.y < 0) { //top
-    ballEntity.velocity.y = Math.abs(ballEntity.velocity.y);
-  }
-
-  if (ballEntity.position.y + ballEntity.size > height - batEntity.size/5-35) { //bunden
-    if (ballEntity.position.x > batEntity.position.x 
-      && ballEntity.position.x < batEntity.position.x + batEntity.size) {
-      ballEntity.velocity.y = -1 * Math.abs(ballEntity.velocity.y);
-    } else if ( ballEntity.position.y + ballEntity.size > height+50) { //game Running
-      startRecording()
-      setIsRunning(false)
-
-    } 
-  }
-
-  //Bat
-  let newPosition = 100
-  if (isAvailable && motionData) {
-
-   newPosition = 250*  motionData.rotation.gamma + 150 // gamma: -1 til +1
-  }
-  if (!isNaN(newPosition)) { //kun hvis newPositoon er et tal, gå videre
-    batEntity.position.x = newPosition
-  }
-
-
-
-  return entities;
-};
-
-async function startRecording(){
-  
-  try {
-  const permission = await Audio.requestPermissionsAsync()
-  if (permission.status === 'granted'){
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS:true,
-      playsInSilentModeIOS:true
-    })
-    const newRecording = new Audio.Recording()
-    await newRecording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY)
-    await newRecording.startAsync()
-    setRecording(newRecording) // er den nødvendig?
-    setTimeout(async() => {
-      if (newRecording) {
-        await newRecording.stopAndUnloadAsync()
-        await Audio.setAudioModeAsync({
-          allowsRecordingIOS:false,
-          playsInSilentModeIOS:true
-        })
-        const uri = newRecording.getURI()
-        setRecording(null)
-        //afspil lyden
-        playSound(uri)
-        
+    async function subscribe() {
+      const available = await DeviceMotion.isAvailableAsync();
+      setIsAvailable(available);
+      //gem i usestate
+      if (available) {
+        //Vi bruger availabe og ikke isAvailable, da den ikke kan nå at sætte og hente den nye værdi
+        DeviceMotion.setUpdateInterval(20); // 20 millisekund pause imellem hver update
+        DeviceMotion.addListener((deviceMotionData) => {
+          setMotionData(deviceMotionData);
+        });
       }
-    }, 2000)
-  }
+    }
+    subscribe();
+    return () => {
+      DeviceMotion.removeAllListeners();
+    };
+  }, []); // skal kun køre en gang derfor [], det er en mekanisme
 
-  } catch (error) {
-    console.log(error)
-  }
-} 
-async function playSound(uri){
-  const {sound} = await Audio.Sound.createAsync(
-    {uri},
-    {shouldPlay: true}
-    )
-  setSound(sound)
-  await sound.playAsync()
-}
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    if (timer > highscore) {
+      setHighscore(timer);
+    }
 
-function playAgain(){
-  setIsRunning(true)
-}
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const update = (entities, { time }) => {
+    const batEntity = entities.bat;
+    const ballEntity = entities.ball;
+    const topBatEntity = entities.topBat;
+
+
+    ballEntity.position.x += ballEntity.velocity.x * time.delta;
+    ballEntity.position.y += ballEntity.velocity.y * time.delta;
+
+    if (ballEntity.position.x < 0) {
+      //venstre side
+      ballEntity.velocity.x = Math.abs(ballEntity.velocity.x);
+    }
+
+    if (ballEntity.position.x + ballEntity.size > width) {
+      //højre side
+      ballEntity.velocity.x = -1 * Math.abs(ballEntity.velocity.x);
+    }
+
+    // Handle ball collision with top bat
+    if (ballEntity.position.y < topBatEntity.size / 5 + 55) {
+      if (
+        ballEntity.position.x > topBatEntity.position.x &&
+        ballEntity.position.x < topBatEntity.position.x + topBatEntity.size
+      ) {
+        ballEntity.velocity.y = -ballEntity.velocity.y;
+      } else if (ballEntity.position.y < 0) {
+        setIsRunning(false); // Game over
+      }
+    }
+
+    if (
+      ballEntity.position.y + ballEntity.size >
+      height - batEntity.size / 5 - 35
+    ) {
+      //bunden
+      if (
+        ballEntity.position.x > batEntity.position.x &&
+        ballEntity.position.x < batEntity.position.x + batEntity.size
+      ) {
+        ballEntity.velocity.y = -1 * Math.abs(ballEntity.velocity.y);
+      } else if (ballEntity.position.y + ballEntity.size > height + 50) {
+        //game Running
+        setIsRunning(false);
+      }
+    }
+
+    //Bat
+    let newPosition = 100;
+    if (isAvailable && motionData) {
+      newPosition = 250 * motionData.rotation.gamma + 150; // gamma: -1 til +1
+    }
+    if (!isNaN(newPosition)) {
+      //kun hvis newPositoon er et tal, gå videre
+      batEntity.position.x = newPosition;
+    }
+
+    topBatEntity.position.x = entities.bat.position.x;
+
+    return entities;
+  };
+
+  function playAgain() {
+    setIsRunning(true);
+    setTimer(0); // Reset timer when the game starts again
+  }
   return (
-    <View style={{flex:1}}>
-    <GameEngine
-      running={isRunning}
-      key={isRunning ? "game running" : "game stopped"}
-      systems={[update]}
-      entities={{ ball, bat }}
-      style={{ flex: 1, backgroundColor: "white" }}
-    />
-    {!isRunning &&
-    <Button title="Play again" onPress={playAgain}></Button>}
-</View>
+    <View style={{ flex: 1 }}>
+      <Text style={styles.timerText}>Timer: {timer}</Text>
+      <Text style={styles.highScoreText}>Highscore: {highscore}</Text>
+      <GameEngine
+        running={isRunning}
+        key={isRunning ? "game running" : "game stopped"}
+        systems={[update]}
+        entities={{ ball, bat, topBat }}
+        style={{ flex: 1, backgroundColor: "white" }}
+      />
+      {!isRunning && (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "white", // Sets the background color of the view
+            position: "absolute", // Position it over the game
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        >
+          <Text style={styles.highScoreText}>Highscore: {highscore}</Text>
+          <Text style={styles.title}>Welcome to the Game!</Text>
+
+          <Button title="Play again" onPress={playAgain} />
+        </View>
+      )}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    color: "black",
+    marginBottom: 20,
+  },
+  playButton: {
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  playButtonText: {
+    color: "black",
+    fontSize: 18,
+  },
+  timerText: {
+    color: "black",
+    fontSize: 20,
+    position: "absolute",
+    top: 40,
+    alignSelf: "center",
+    zIndex: 1,
+  },
+  highScoreText: {
+    color: "black",
+    fontSize: 20,
+    position: "absolute",
+    top: 80,
+    alignSelf: "center",
+    zIndex: 1,
+  },
+});
